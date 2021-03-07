23d8102f3c0d9b84e1b3e2f9cac10e1b2da46d52
leeyoungak <slingstone7@gmail.com>
2021-03-08 01:13:24 +0900

    Posts 조회/수정 API

--- src/main/java/net/sling/book/springboot/service/PostsService.java
+++ src/main/java/net/sling/book/springboot/service/PostsService.java
@@ -1,9 +1,13 @@
 package net.sling.book.springboot.service;
 
 import lombok.RequiredArgsConstructor;
+import net.sling.book.springboot.domain.posts.Posts;
 import net.sling.book.springboot.domain.posts.PostsRepository;
+import net.sling.book.springboot.web.dto.PostsResponseDto;
 import net.sling.book.springboot.web.dto.PostsSaveRequestDto;
+import net.sling.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,5 +17,22 @@ public class PostsService {
 
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/net/sling/book/springboot/web/PostsApiController.java
+++ src/main/java/net/sling/book/springboot/web/PostsApiController.java
@@ -2,11 +2,10 @@ package net.sling.book.springboot.web;
 
 import lombok.RequiredArgsConstructor;
 import net.sling.book.springboot.service.PostsService;
+import net.sling.book.springboot.web.dto.PostsResponseDto;
 import net.sling.book.springboot.web.dto.PostsSaveRequestDto;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import net.sling.book.springboot.web.dto.PostsUpdateRequestDto;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,6 +16,16 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
     }
 
 
